20b792ffa1d85525abb31482b68b01daa48449d3
William Zerega <Bill@Williams-MacBook-Pro-7.local>
2017-10-02 20:13:21 -0400

    completed Composition program

---
+++ src/com/BillZ/Stove.java
@@ -0,0 +1,36 @@
+package com.BillZ;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+/**
+ * Created by Bill on 5/26/17.
+ */
+public class Stove {
+    private int burners;
+    private int currentTemp;
+    private boolean electric;
+
+    public Stove(int burners, int currentTemp, boolean electric) {
+        this.burners = burners;
+        this.currentTemp = currentTemp;
+        this.electric = electric;
+    }
+
+    public int getBurners() {
+        return burners;
+    }
+
+    public int getCurrentTemp() {
+        return currentTemp;
+    }
+
+    public boolean isElectric() {
+        return electric;
+    }
+
+    public void turnOnBurners(){
+        System.out.println("The burners are burning!");
+    }
+
+
+}
---
+++ src/com/BillZ/Vehicle.java
@@ -0,0 +1,12 @@
+package com.BillZ;
+
+/**
+ * Created by Bill on 5/26/17.
+ */
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
